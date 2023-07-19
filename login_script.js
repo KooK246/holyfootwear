2244f6d1b48cb04833d5a234eeb411ceaf386eee
KooK246 <137267907+KooK246@users.noreply.github.com>
2023-07-19 21:38:08 +0530

    Add files via upload

--- login_script.js
+++ login_script.js
@@ -3,6 +3,8 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+var passCheck = 0;
+var mailCheck = 0;
 
 // show input error message
 function showError(input, message) {
@@ -23,8 +25,9 @@ function checkEmail(input) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     showSuccess(input);
+    mailCheck = 1;
   } else {
-    showError(input, 'Enail is not valid');
+    showError(input, 'E-mail is not valid');
   }
 }
 
@@ -32,20 +35,19 @@ function checkEmail(input) {
 function checkRequired(inputArr) {
   inputArr.forEach(function (input) {
     if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
+      showError(input, `${fieldCheck(input)} is required`);
     } else {
       showSuccess(input);
-      
     }
   });
 }
 
 //check input lenght
-function checkLength(input, min, max) {
+function lengthCheck(input, min, max) {
   if (input.value.length < min) {
-    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    showError(input, `${fieldCheck(input)} must be at least ${min} characters`);
   } else if (input.value.length > max) {
-    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    showError(input, `${fieldCheck(input)} must be less than ${max} characters`);
   } else {
     showSuccess(input);
   }
@@ -53,24 +55,35 @@ function checkLength(input, min, max) {
 
 // check passwords match
 
-function checkPasswordsMatch(input1, input2) {
+function passwordCheck(input1, input2) {
   if (input1.value !== input2.value) {
     showError(input2, 'Passwords do not match');
+  }
+  if ((input1.value == input2.value) && (input1.value.length >= 6)) {
+    passCheck = 1;
   }
 }
 
 // Get fieldname
-function getFieldName(input) {
+function fieldCheck(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
+
+
 
 // Event listeners
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
-  checkLength(username, 3, 15);
-  checkLength(password, 6, 25);
+  lengthCheck(username, 3, 15);
+  lengthCheck(password, 6, 25);
   checkEmail(email);
-  checkPasswordsMatch(password, password2);
+  passwordCheck(password, password2);
+
+  if ((passCheck = 1) && (mailCheck == 1)) {
+    window.location.href = "https://keen-pudding-2e2d69.netlify.app";
+  }
+
 });
+
